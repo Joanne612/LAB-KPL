a1262c7b0a36db174c2c43798f35bb692872547c
Joanne612 <joanneagnesian@gmail.com>
2026-03-08 15:30:07 +0700

    Menambahkan tampilan GUI kalkulator

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,179 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            label_output = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            SuspendLayout();
+            // 
+            // label_output
+            // 
+            label_output.AutoSize = true;
+            label_output.BackColor = Color.White;
+            label_output.Location = new Point(504, 129);
+            label_output.Name = "label_output";
+            label_output.Size = new Size(153, 32);
+            label_output.TabIndex = 0;
+            label_output.Text = "Label Output";
+            label_output.Click += label_output_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(463, 190);
+            button1.Name = "button1";
+            button1.Size = new Size(80, 76);
+            button1.TabIndex = 1;
+            button1.Text = "1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(541, 190);
+            button2.Name = "button2";
+            button2.Size = new Size(80, 76);
+            button2.TabIndex = 2;
+            button2.Text = "2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(618, 190);
+            button3.Name = "button3";
+            button3.Size = new Size(80, 76);
+            button3.TabIndex = 3;
+            button3.Text = "3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(463, 263);
+            button4.Name = "button4";
+            button4.Size = new Size(80, 76);
+            button4.TabIndex = 4;
+            button4.Text = "4";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(541, 263);
+            button5.Name = "button5";
+            button5.Size = new Size(80, 76);
+            button5.TabIndex = 5;
+            button5.Text = "5";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(618, 263);
+            button6.Name = "button6";
+            button6.Size = new Size(80, 76);
+            button6.TabIndex = 6;
+            button6.Text = "6";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(463, 336);
+            button7.Name = "button7";
+            button7.Size = new Size(80, 76);
+            button7.TabIndex = 7;
+            button7.Text = "7";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(541, 336);
+            button8.Name = "button8";
+            button8.Size = new Size(80, 76);
+            button8.TabIndex = 8;
+            button8.Text = "8";
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(618, 336);
+            button9.Name = "button9";
+            button9.Size = new Size(80, 76);
+            button9.TabIndex = 9;
+            button9.Text = "9";
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            button10.Location = new Point(463, 410);
+            button10.Name = "button10";
+            button10.Size = new Size(80, 76);
+            button10.TabIndex = 10;
+            button10.Text = "+";
+            button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            button11.Location = new Point(541, 410);
+            button11.Name = "button11";
+            button11.Size = new Size(80, 76);
+            button11.TabIndex = 11;
+            button11.Text = "0";
+            button11.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            button12.Location = new Point(618, 410);
+            button12.Name = "button12";
+            button12.Size = new Size(80, 76);
+            button12.TabIndex = 12;
+            button12.Text = "=";
+            button12.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1172, 804);
+            Controls.Add(button12);
+            Controls.Add(button11);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label_output);
+            Name = "Form1";
             Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label_output;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button12;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,20 @@ namespace TP_MODUL3_103022400114
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label_output_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
